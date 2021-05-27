bd6e9c074a6a4f2dd04c2c6b6dc4c2f62cf184ba
sana <takagisou@users.noreply.github.com>
2021-05-28 02:16:58 +0900

    init

---
+++ Memorize/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Memorize
+//
+//  Created by sana on 2021/05/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by sana on 2021/05/28.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
