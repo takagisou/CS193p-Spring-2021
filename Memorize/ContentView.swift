e48bad5813ae4a995e9d145cfb4e56b57f0d8653
sana <takagisou@users.noreply.github.com>
2021-05-28 03:35:47 +0900

    done: Lecture 1: Getting started with SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        return ZStack {
+            RoundedRectangle(cornerRadius: 20)
+                .stroke(lineWidth: 3)
+            
+            Text("Hello world!")
+                .padding()
+        }
+        .padding(.horizontal)
+        .foregroundColor(.red)
     }
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+# CS193P Spring 2021
+
 https://www.youtube.com/playlist?list=PLpGHT1n4-mAsxuRxVPv7kj4-dQYoC3VVu
+
+# Homework
+https://cs193p.sites.stanford.edu/
