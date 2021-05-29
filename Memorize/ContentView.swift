5539dff41503f218da16ce99fead55b5e0b948ac
sana <takagisou@users.noreply.github.com>
2021-05-30 04:12:55 +0900

    add buttons

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -8,23 +8,64 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var emojis = [
+        "🚲", "🚂", "🚁", "🚜", "🚕", "🏎",
+        "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️",
+        "🛸", "🛶", "🚌", "🏍", "🛺", "🚠",
+        "🛵", "🚗", "🚚", "🚇", "🛻", "🚝",
+    ]
+    
+    @State
+    var emojiCount = 6
+    
     var body: some View {
-        HStack {
-            CardView()
-            CardView()
-            CardView()
-            CardView()
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
+                    CardView(content: emoji)
+                })
+            }
+            Spacer(minLength: 20)
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
+            .padding(.horizontal)
         }
         .padding(.horizontal)
         .foregroundColor(.red)
+    }
+    
+    var remove: some View {
+        Button(action: {
+            if 1 < emojiCount {
+                emojiCount -= 1
+            }
+        }, label: {
+            Image(systemName: "minus.circle")
+        })
+    }
+    
+    var add: some View {
+        Button(action: {
+            if emojiCount < emojis.count {
+                emojiCount += 1
+            }
+        }, label: {
+            Image(systemName: "plus.circle")
+        })
     }
 }
 
 
 struct CardView: View {
-
+    var content: String
+    
     @State
-    var isFaceUp = false
+    var isFaceUp = true
     
     var body: some View {
         let shape = RoundedRectangle(cornerRadius: 20)
@@ -32,7 +73,7 @@ struct CardView: View {
             if isFaceUp {
                 shape.fill().foregroundColor(.white)
                 shape.stroke(lineWidth: 3)
-                Text("✈️").font(.largeTitle)
+                Text(content).font(.largeTitle)
             } else {
                 shape.fill()
             }
--- README.md
+++ README.md
@@ -4,3 +4,22 @@ https://www.youtube.com/playlist?list=PLpGHT1n4-mAsxuRxVPv7kj4-dQYoC3VVu
 
 # Homework
 https://cs193p.sites.stanford.edu/
+
+# Lectture2 
+
+4 emojis
+
+```swift
+var emojis = ["🚗","🚕","🚙", "🚌"]
+```
+
+24 vehicales
+
+```swfit
+var emojis = [
+    "🚲", "🚂", "🚁", "🚜", "🚕", "🏎",
+    "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️",
+    "🛸", "🛶", "🚌", "🏍", "🛺", "🚠",
+    "🛵", "🚗", "🚚", "🚇", "🛻", "🚝",
+]
+```
