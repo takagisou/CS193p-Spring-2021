d9eda9ea839942f6beae2a46d79063f3f844b741
sana <takagisou@users.noreply.github.com>
2021-05-29 19:16:56 +0900

    Lecture 2: Learning more about SwiftUI

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -9,15 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        return ZStack {
-            RoundedRectangle(cornerRadius: 20)
-                .stroke(lineWidth: 3)
-            
-            Text("Hello world!")
-                .padding()
+        HStack {
+            CardView()
+            CardView()
+            CardView()
+            CardView()
         }
         .padding(.horizontal)
         .foregroundColor(.red)
+    }
+}
+
+
+struct CardView: View {
+
+    @State
+    var isFaceUp = false
+    
+    var body: some View {
+        let shape = RoundedRectangle(cornerRadius: 20)
+        ZStack {
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.stroke(lineWidth: 3)
+                Text("✈️").font(.largeTitle)
+            } else {
+                shape.fill()
+            }
+        }
+        .onTapGesture {
+            isFaceUp.toggle()
+        }
     }
 }
 
@@ -25,6 +47,9 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
+                .preferredColorScheme(.dark)
+            ContentView()
+                .preferredColorScheme(.light)
         }
     }
 }
