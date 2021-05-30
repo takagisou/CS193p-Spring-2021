e56456f67bbb94d38ca3a0fef5ff9cd305ac641b
sana <takagisou@users.noreply.github.com>
2021-05-30 10:34:20 +0900

    Add a title "Memorize!" to the top of the screen.

--- Memorize/ContentView.swift
+++ Memorize/ContentView.swift
@@ -21,6 +21,7 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
+            Text("Memorize!").font(.title)
             ScrollView {
                 LazyVGrid(columns: [GridItem(.adaptive(minimum: 65))]){
                     ForEach(emojis[0..<emojiCount], id: \.self, content: { emoji in
