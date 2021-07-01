71080b59f915c02e90c8214a524f0edee31d04bf
sana <takagisou@users.noreply.github.com>
2021-07-02 05:42:54 +0900

    clipped

--- EmojiArt/EmojiArt/PaletteChooser.swift
+++ EmojiArt/EmojiArt/PaletteChooser.swift
@@ -21,6 +21,7 @@ struct PaletteChooser: View {
             paletteControlButton
             body(for: store.palette(at: chosenPaletteIndex))
         }
+        .clipped()
     }
     
     var paletteControlButton: some View {
