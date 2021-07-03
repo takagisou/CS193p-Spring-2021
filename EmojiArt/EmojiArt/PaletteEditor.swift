d9f69e5bff88d088d375bb28eae6e77808a9a88c
sana <takagisou@users.noreply.github.com>
2021-07-03 22:50:46 +0900

    add, remove emoji

--- EmojiArt/EmojiArt/PaletteEditor.swift
+++ EmojiArt/EmojiArt/PaletteEditor.swift
@@ -10,20 +10,64 @@ import SwiftUI
 struct PaletteEditor: View {
     
     @Binding var palette: Palette
+    @State private var emojisToAdd = ""
     
     var body: some View {
         Form {
-            TextField("Name", text: $palette.name)
+            nameSection
+            addEmojiSection
+            removeEmojiSection
         }
         .frame(minWidth: 300, minHeight: 350)
     }
+    
+    var nameSection: some View {
+        Section(header: Text("Name")) {
+            TextField("Name", text: $palette.name)
+        }
+    }
+    
+    var addEmojiSection: some View {
+        Section(header: Text("Add Emojis")) {
+            TextField("", text: $emojisToAdd)
+                .onChange(of: emojisToAdd) { emojis in
+                    addEmojis(emojis)
+                }
+        }
+    }
+    
+    var removeEmojiSection: some View {
+        Section(header: Text("Remove Emoji")) {
+            let emojis = palette.emojis.removingDuplicateCharacters.map { String($0) }
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 40))]) {
+                ForEach(emojis, id: \.self) { emoji in
+                    Text(emoji)
+                        .onTapGesture {
+                            withAnimation {
+                                palette.emojis.removeAll(where: { String($0) == emoji })
+                            }
+                        }
+                }
+            }
+        }
+    }
+    
+    func addEmojis(_ emojis: String) {
+        withAnimation {
+            palette.emojis = (emojis + palette.emojis)
+                .filter { $0.isEmoji }
+                .removingDuplicateCharacters
+        }
+    }
+    
 }
 
 
 struct PaletteEditor_Previews: PreviewProvider {
     static var previews: some View {
-        Text("Fix Me!")
-//        PaletteEditor()
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 4)))
             .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/350.0/*@END_MENU_TOKEN@*/))
+        PaletteEditor(palette: .constant(PaletteStore(named: "Preview").palette(at: 2)))
+            .previewLayout(.fixed(width: /*@START_MENU_TOKEN@*/300.0/*@END_MENU_TOKEN@*/, height: 600)) 
     }
 }
